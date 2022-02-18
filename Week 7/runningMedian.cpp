0798048a96ad26a65c9eb211b1299bb78f1e4ab1
Mohammad Maruf Imtiaze <mimtiaze@gmail.com>
2022-02-18 12:05:06 +0600

    running median

--- Week 7/runningMedian.cpp
+++ Week 7/runningMedian.cpp
@@ -7,7 +7,7 @@ int main() {
     priority_queue <int> higher;
     priority_queue <int, vector<int>, greater<int>> lower;
 
-    while(1){
+    while(true){
         int num;
         cin >> num;
         if(num == -1) break;
@@ -16,10 +16,24 @@ int main() {
         higher.push(lower.top());
         lower.pop();
 
-        while(higher.size()>lower.size()+1) {
+        while(higher.size()>lower.size()) {
             lower.push(higher.top());
             higher.pop();
         }
+
+        //Finding median
+        int med = -1;
+        if(lower.size()>higher.size()){
+            med = lower.top();
+        } else if(higher.size()==lower.size()) {
+            med = (higher.top() + lower.top()) / 2;
+        }
+
+        (med==-1)? cout << "Something is wrong!!!" << endl : cout << med << endl;
+        //cout << med << endl;
+        //cout << "SIZE: " <<higher.size() << " " << lower.size() << endl;
+        //cout << "TOP: " << higher.top() << " " << lower.top() << endl;
+
     }
 
     return 0;
